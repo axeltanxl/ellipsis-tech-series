fa5ee7e2d32b99cc6492f3331b6f14ec159f9b9f
nicolegabriellet <80974220+nicolegabriellet@users.noreply.github.com>
2023-08-26 02:35:55 +0800

    feat: created general home page layout

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
+        "light_orange": "#FFE3C5",
+        "light_purple": "#D9D9FF",
+        "light_green": "#C8F1E0",
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
