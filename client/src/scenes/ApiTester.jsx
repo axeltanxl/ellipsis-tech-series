3e1c7385001c70dca948d66ac491ae65a8e6db49
Rui Yan <angruiyan@gmail.com>
2023-08-25 23:39:37 +0800

    Added NLP logging api endpoint; Added restaurant food results

--- client/src/scenes/ApiTester.jsx
+++ client/src/scenes/ApiTester.jsx
@@ -1,17 +1,70 @@
 import { Box, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { searchFood, searchNutritionByFoodName } from '../services/foodService';
+import { searchFood, searchNutritionByFoodName, searchFoodWithNLP } from '../services/foodService';
 
 function FoodSearch() {
+	const [foods, setFoods] = useState([]);
+	const [restaurants, setRestaurants] = useState([]);
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const nutritionData = await searchFood('jollibee');
+				console.log(JSON.stringify(nutritionData));
+				const commonFoods = nutritionData.common || [];
+				const brandedFoods = nutritionData.branded || [];
+
+				setFoods(commonFoods);
+				setRestaurants(brandedFoods);
+			} catch (error) {
+				console.log('Error:', error);
+			}
+		}
+
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			<h1>List of Food Names</h1>
+			<ul>
+				{foods.map((food, index) => (
+					<li key={index}>
+						<a
+							href="#"
+							onClick={() => searchNutritionByFoodName(food.food_name)}
+						>
+							{food.food_name}
+						</a>
+						{food.something && <span> - {food.something}</span>}
+					</li>
+				))}
+			</ul>
+			{restaurants.length > 0 && (
+				<div>
+					<h2>Restaurant</h2>
+					<ul>
+						{restaurants.map((restaurant, index) => (
+							<li key={index}>
+								{restaurant.brand_name} - {restaurant.food_name}
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
+}
+
+function NLPSearch() {
 	const [foods, setFoods] = useState([]);
 	useEffect(() => {
 		async function fetchData() {
 			try {
-				const nutritionData = await searchFood('kway teow');
+				const nutritionData = await searchFoodWithNLP('2 eggs and 1 bacon');
 				console.log(JSON.stringify(nutritionData));
-				const commonFoods = nutritionData.common || [];
+				const food = nutritionData.foods;
 
-				setFoods(commonFoods);
+				setFoods(food);
 			} catch (error) {
 				console.log('Error:', error);
 			}
@@ -42,6 +95,8 @@ function FoodSearch() {
 const ApiTester = () => {
 	return (
 		<Box>
+			<Typography>API tester</Typography>
+			<NLPSearch />
 			<Typography>API tester</Typography>
 			<FoodSearch />
 		</Box>
--- client/src/services/foodService.js
+++ client/src/services/foodService.js
@@ -44,3 +44,30 @@ export const searchNutritionByFoodName = async (query) => {
 		throw error;
 	}
 };
+
+export const searchFoodWithNLP = async (query) => {
+	let data = JSON.stringify({
+		query: query,
+		timezone: 'Asia/Singapore',
+	});
+
+	let config = {
+		method: 'post',
+		url: `https://trackapi.nutritionix.com/v2/natural/nutrients`,
+		headers: {
+			'content-type': 'application/json',
+			accept: 'application/json',
+			'x-app-id': process.env.NUTRITIONIX_API_ID,
+			'x-app-key': process.env.NUTRITIONIX_API_KEY,
+		},
+		data,
+	};
+
+	try {
+		const response = await axios.request(config);
+		return response.data;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+};
