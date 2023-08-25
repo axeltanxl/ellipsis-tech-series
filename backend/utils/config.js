e36f6b0db802644226b35476275bb8aac8eff980
Axel Tan <axeltanxl@gmail.com>
2023-08-25 23:05:34 +0800

    feat: Add register functionality

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const EXPIRY = process.env.EXPIRY;
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  ACCESS_TOKEN_SECRET,
+  EXPIRY
 }
