30f42b2cfefcb5969aae3dff0c53f9999a00eb9f
Axel Tan <axeltanxl@gmail.com>
2023-08-26 18:01:53 +0800

    feat: get current sodium level for a user

--- backend/routes/mealRoutes.js
+++ backend/routes/mealRoutes.js
@@ -7,6 +7,7 @@ router.post('/create', mealController.logMeal);
 router.get('/getMine', mealController.getMyMeals);
 router.get('/mealLocations', mealController.getMealLocations);
 router.post('/filterDateRange', mealController.getDurationMeals);
+router.get('/sodiumIntakeLevel', mealController.getCurrentSodiumLevel);
 router.get('/:mealId', mealController.getMeal)
 router.delete('/:mealId', mealController.removeMeal);
 
