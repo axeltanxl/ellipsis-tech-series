72c91a5328aec72fc2e7022e97caa1235f4c0abc
nicolegabriellet <80974220+nicolegabriellet@users.noreply.github.com>
2023-08-26 12:10:27 +0800

    refactor: changed format of home page code

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import App from "../App";
+import Home from "../pages/HomePage";
 import Nearby from "../pages/NearbyPage";
 import Food from "../pages/FoodPage";
 import Profile from "../pages/ProfilePage";
 
 const AppRoutes = () => (
   <Routes>
-    <Route exact path="/" element={<App />} />
+    <Route exact path="/" element={<Home />} />
     <Route exact path="/nearby" element={<Nearby />} />
     <Route exact path="/food" element={<Food />} />
     <Route exact path="/profile" element={<Profile />} />
