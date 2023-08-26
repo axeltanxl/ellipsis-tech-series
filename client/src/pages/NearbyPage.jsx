68af3fc782423a8922950cb6c40887c0e880b13e
Rui Yan <angruiyan@gmail.com>
2023-08-26 22:29:54 +0800

    feat: static markers are showing on map

--- client/src/pages/NearbyPage.jsx
+++ client/src/pages/NearbyPage.jsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState, Component, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Layout from '../components/Layout';
 import Banner from '../components/maps/Banner';
-import { getNearbyPlaces } from '../services/mapService';
-import ReactSearchBox from 'react-search-box';
-import Place from '../components/maps/Place';
+import { fuzzySearchFoodByLocation } from '../services/foodService';
 
 import '@tomtom-international/web-sdk-maps/dist/maps.css';
 import tt from '@tomtom-international/web-sdk-maps';
@@ -11,25 +9,69 @@ import tt from '@tomtom-international/web-sdk-maps';
 const Nearby = () => {
 	const mapElement = useRef(null);
 
-	const [geoLocation, setGeoLocation] = useState({ latitude: 1.296568, longitude: 103.852119 }); // Default location
+	const [geoLocation, setGeoLocation] = useState({ latitude: 1.296568, longitude: 103.852119 });
 	const [geoError, setGeoError] = useState(null);
+	const [searchQuery, setSearchQuery] = useState('');
+	const [markers, setMarkers] = useState([]);
 
 	useEffect(() => {
-		let map = tt.map({
-			key: import.meta.env.VITE_TOMTOM_API_KEY,
-			container: mapElement.current,
-			center: [geoLocation.longitude, geoLocation.latitude],
-			zoom: 13,
-		});
-		return () => map.remove();
+		if (geoLocation.latitude && geoLocation.longitude) {
+			const map = tt.map({
+				key: import.meta.env.VITE_TOMTOM_API_KEY, // Use your API key
+				container: mapElement.current,
+				center: [geoLocation.longitude, geoLocation.latitude],
+				zoom: 13,
+			});
+
+			setMarkers([]);
+			addMarkers(map);
+
+			return () => {
+				map.remove();
+			};
+		}
 	}, [geoLocation]);
+
+	const addMarkers = async (map) => {
+		let finalResult = await fuzzySearchFoodByLocation(
+			searchQuery,
+			geoLocation.longitude,
+			geoLocation.latitude
+		);
+		const locations = finalResult.restaurants.flatMap((restaurant) => restaurant.locations);
+
+		const newMarkers = locations.map((location) => {
+			const marker = new tt.Marker()
+				.setLngLat([location.position.lon, location.position.lat])
+				.addTo(map);
+
+			return marker;
+		});
+
+		setMarkers(newMarkers);
+	};
+
+	const handleSearch = () => {
+		// Update markers based on the search query
+		if (searchQuery) {
+			// Clear existing markers
+			markers.forEach((marker) => marker.remove());
+			setMarkers([]);
+
+			// Add new markers based on the search query
+			addMarkers(mapElement.current.getMap());
+		}
+	};
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(
 			(e) => {
-				setGeoLocation(e.coords);
+				setGeoLocation({
+					latitude: e.coords.latitude,
+					longitude: e.coords.longitude,
+				});
 			},
-			async (err) => {
+			(err) => {
 				setGeoError(err);
 			}
 		);
@@ -41,7 +83,22 @@ const Nearby = () => {
 				geoLocation={geoLocation}
 				geoError={geoError}
 			/>
-
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					marginBottom: '20px',
+				}}
+			>
+				<input
+					type="text"
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+					placeholder="Enter your query..."
+				/>
+				<button onClick={handleSearch}>Search</button>
+			</div>
 			<div
 				ref={mapElement}
 				style={{ width: '100%', height: '400px' }}
