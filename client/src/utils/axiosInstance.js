6021123493d33b4f4ba1ade8f994f1ddd9572b0c
Axel Tan <axeltanxl@gmail.com>
2023-08-27 11:34:32 +0800

    refactor: add connection to prod backend url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-
-
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: `http://${import.meta.env.VITE_BASE_URL_PROD}/api`,
   headers: { "Access-Control-Allow-Origin": "*" },
   withCredentials: false,
 });
-
-
-
